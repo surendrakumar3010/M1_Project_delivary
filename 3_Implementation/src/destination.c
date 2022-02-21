af236376323ab91970cf10a5c0a161c61907ea54
surendrakumar3010 <93932674+surendrakumar3010@users.noreply.github.com>
2022-02-21 14:50:06 +0530

    Update destination.c

--- 3_Implementation/src/destination.c
+++ 3_Implementation/src/destination.c
@@ -3,92 +3,92 @@ void destination()
 	system("cls");
 	int n;
 	printf("\n\t DESTINATION :");
-	printf("\n\n\t 1. ITALY");
-	printf("\n\n\t 2. FRANCE");
-	printf("\n\n\t 3. LONDON");	printf("\n\n\t 4. SYDNEY");
-	printf("\n\n\t 5. NEW YORK");
-	printf("\n\n\t 6. TOKYO");
-	printf("\n\n\t 7. SHIMLA");
+	printf("\n\n\t 1. TELANGANA");
+	printf("\n\n\t 2. ANDHRAPRADESH");
+	printf("\n\n\t 3. GOA");	printf("\n\n\t 4. KARNATAKA");
+	printf("\n\n\t 5. MAHARASHTRA");
+	printf("\n\n\t 6. PUNJAB");
+	printf("\n\n\t 7. TAMILNADU");
 		printf("\n\n\t ENTER CHOICE :  ");
 	scanf("%d", &n);
 	switch (n)
 	{
-	case 1:italy();break;
-	case 2:france();break;
-	case 3:london();break;
-	case 4:sydney();break;
-	case 5:newyork();break;
-	case 6:tokyo();break;
-	case 7:shimla();break;
+	case 1:telangana();break;
+	case 2:andhrapradesh();break;
+	case 3:goa();break;
+	case 4:karnataka();break;
+	case 5:maharashtra();break;
+	case 6:punjab();break;
+	case 7:tamilnadu();break;
 	}
 }
 
 
-void italy()
+void telangana()
 {
 	int cnt;
 	system("cls");
-	printf("\n\t\tWELCOME TO ITALY TOURISM  !!!\n");
+	printf("\n\t\tWELCOME TO TELANGANA TOURISM  !!!\n");
 printf("\n\t\t    ENTER NO. OF PEOPLE ");
     scanf("%d",&cnt);
 	input(cnt,1);	
 }
 
-void france()
+void andhrapradesh()
 {
 	int  cnt;
 	system("cls");	
-	printf("\n\t\tWELCOME TO FRANCE TOURISM  !!!\n");
+	printf("\n\t\tWELCOME TO ANDHRAPRADESH TOURISM  !!!\n");
 printf("\n\t\t    ENTER NO. OF PEOPLE ");
     scanf("%d",&cnt);
 	input(cnt,2);	
 }
 
-void london()
+void goa()
 {
 	int  cnt;
 	system("cls");
-	printf("\n\t\tWELCOME TO LONDON TOURISM  !!!\n");
+	printf("\n\t\tWELCOME TO GOA TOURISM  !!!\n");
 printf("\n\t\t    ENTER NO. OF PEOPLE ");
     scanf("%d",&cnt);
 	input(cnt,3);	
 }
 
-void sydney()
+void karnataka()
 {
 	int  cnt;
 	system("cls");
-	printf("\n\t\tWELCOME TO SYDNEY TOURISM  !!!\n");
+	printf("\n\t\tWELCOME TO KARNATAKA TOURISM  !!!\n");
 printf("\n\t\t    ENTER NO. OF PEOPLE ");
     scanf("%d",&cnt);
 	input(cnt,4);	
 }
 
-void newyork()
+void maharashtra()
 {
 	int  cnt;
 	system("cls");
-	printf("\n\t\tWELCOME TO NEW YORK TOURISM  !!!\n");
+	printf("\n\t\tWELCOME TO MAHARASHTRA TOURISM  !!!\n");
 printf("\n\t\t    ENTER NO. OF PEOPLE ");
     scanf("%d",&cnt);
 	input(cnt,5);	
 }
 
-void tokyo()
+void punjab()
 {
 	int  cnt;
 system("cls");
-	printf("\n\t\tWELCOME TO TOKY0 TOURISM  !!!\n");
+	printf("\n\t\tWELCOME TO PUNJAB TOURISM  !!!\n");
 printf("\n\t\t    ENTER NO. OF PEOPLE ");
     scanf("%d",&cnt);
 	input(cnt,6);	
 }
 
-void shimla()
+void tamilnadu()
 {
 	int  cnt;
 	system("cls");
-	printf("\n\t\tWELCOME TO SHIMLA TOURISM  !!!\n");
+	printf("\n\t\tWELCOME TO TAMILNADU TOURISM  !!!\n");
 printf("\n\t\t    ENTER NO. OF PEOPLE ");
     scanf("%d",&cnt);
 	input(cnt,7);	
