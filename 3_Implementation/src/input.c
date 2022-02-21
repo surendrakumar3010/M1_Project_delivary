43ae849f1c0a348000b78fd9f86c3f228dfe13df
surendrakumar3010 <93932674+surendrakumar3010@users.noreply.github.com>
2022-02-21 14:05:44 +0530

    Add files via upload

---
+++ 3_Implementation/src/input.c
@@ -0,0 +1,49 @@
+void input(int cnt,int cno)
+{
+	int a[cnt];	
+	int pos=0,res;
+	switch(cno)
+	{
+		case 1:strcpy(i.destination,     "ITALY");break;
+		case 2:strcpy(i.destination,     "FRANCE");break;
+		case 3:strcpy(i.destination,     "LONDON");break;
+		case 4:strcpy(i.destination,    "SYDNEY" );break;
+		case 5:strcpy(i.destination,     "NEW_YORK");break;
+		case 6:strcpy(i.destination,     "TOKYO");break;
+		case 7:strcpy(i.destination,    "SHIMLA" );break;
+	}
+	int x=0,id;
+	while(x<cnt)
+		{  
+		system("cls");
+			printf("\n\t\t ENTER %d PERSON DETAILS",x+1);
+		a:
+		printf("\n\n\t     CHOOSE YOUR TRAVEL ID (1-100) : ");
+	scanf("%d",&id);
+	res=check(id);
+	if(res==1)
+	goto a;
+	printf("\n\t    ENTER  NAME , AGE , GENDER (M/F)  :\n\n");
+	FILE *f;
+	f=fopen("info.txt","a+");
+	printf("\t    ");
+	scanf("%s", &i.name);
+	printf("\t    ");
+	scanf("%d",&i.age);
+	printf("\t    ");
+	getchar();
+    i.gender=getchar();
+    printf("\n\n\t\t   press enter to continue .....");
+     getchar();
+	getchar();	
+	flights(id);
+	hotels();
+	fprintf(f,"%d %s %d %c %d %s %s %f %s ",id,i.name,i.age,i.gender,i.cost,i.destination,i.fname,i.time,i.shift);
+	fprintf(f," %d %d %d %d %d %d %s\n",d,m,y,d1,m1,y1,i.hname);
+      fclose(f);
+      a[x]=id;
+      ++x;
+		}
+	print_ticket(a,cnt);
+}
+
